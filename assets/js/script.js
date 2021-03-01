ab22052533ddea5cc4a53218cd9787963cd26a1d
ezzeldem <ezzelden06@gmail.com>
2021-03-02 01:36:20 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // intro slider
+
+    if ($("html").attr("dir") == "rtl") {
+        var dir_lang = true;
+    } else {
+        var dir_lang = false;
+    }
+});
